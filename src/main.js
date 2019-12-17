011a90e4d24bd41667bdb890d048c4fe6283266b
yusuke <yu-yanai@celalink.jp>
2019-12-17 13:13:29 +0900

    :recycle:設定を環境変数から取得できるように修正

--- src/main.js
+++ src/main.js
@@ -4,27 +4,52 @@ import vuetify from './plugins/vuetify';
 import router from './router'
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
+import * as functions from 'firebase-functions';
 
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appID: "app-id",
-  timestampsInSnapshots: true,
-};
+// 本番環境（Firebase 環境変数を使用） = true
+const isProd = false;
+if (isProd) {
+  const apiKey = encodeURIComponent(functions.config().config.app_id)
+  const authDomain = encodeURIComponent(functions.config().config.authdomain)
+  const databaseURL = encodeURIComponent(functions.config().config.database_url)
+  const projectId = encodeURIComponent(functions.config().config.project_id)
+  const storageBucket = encodeURIComponent(functions.config().config.storage_bucket)
+  const messagingSenderId = encodeURIComponent(functions.config().config.messaging_sender_id)
+  const appId = encodeURIComponent(functions.config().config.app_id)
+  const measurementId = encodeURIComponent(functions.config().config.measurement_id)
+
+  const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId,
+    timestampsInSnapshots: true,
+  };
+  firebase.initializeApp(firebaseConfig);
+} else {
+  const firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appID: "app-id",
+    timestampsInSnapshots: true,
+  }; 
+  firebase.initializeApp(firebaseConfig);
+}
 
 Vue.config.productionTip = false
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
